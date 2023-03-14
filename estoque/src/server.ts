6989c4e331e5d69f739be20bb12519de2af840fb
jonasalberton <jonasceolin@gmail.com>
2023-03-14 19:09:36 -0300

    feat: stoque microservice

---
+++ estoque/src/server.ts
@@ -0,0 +1,13 @@
+import Fastify from "fastify"
+import cors from '@fastify/cors'
+import { appRoutes } from "./routes"
+
+const PORT = 3444
+const app = Fastify()
+
+app.register(cors)
+app.register(appRoutes)
+
+
+app.listen({ port: PORT }).then(() => console.log(`Server running at http://localhost:${PORT}`)
+)
